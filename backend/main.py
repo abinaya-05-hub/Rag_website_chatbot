265862106b56ae260b49d7aee24248d348c49ed7
abinaya-05-hub <abinayas23ads@srishakthi.ac.in>
2026-06-23 23:53:48 +0530

    updated the main and scraper

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,9 @@
-from fastapi import FastAPI
-from scraper import scrape_website
-
-app = FastAPI()
-
-
-@app.get("/")
-def home():
-    return {
-        "message": "RAG Website Chatbot Backend Running"
-    }
-
-
 @app.get("/scrape")
 def scrape(url: str):
 
-    content = scrape_website(url)
+    pages = scrape_website(url)
 
     return {
-        "content": content[:1000]
+        "pages_scraped": len(pages),
+        "sample": pages[0][:1000] if pages else ""
     }
--- backend/scraper.py
+++ backend/scraper.py
@@ -1,22 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
 
+visited = set()
 
-def scrape_website(url):
-    try:
-        response = requests.get(url)
+def scrape_website(url, max_pages=10):
 
-        soup = BeautifulSoup(
-            response.text,
-            "html.parser"
-        )
+    pages_content = []
 
-        text = soup.get_text(
-            separator=" ",
-            strip=True
-        )
+    def crawl(current_url):
 
-        return text
+        if len(visited) >= max_pages:
+            return
 
-    except Exception as e:
-        return f"Error: {str(e)}"
+        if current_url in visited:
+            return
+
+        visited.add(current_url)
+
+        try:
+            response = requests.get(current_url, timeout=10)
+
+            soup = BeautifulSoup(
+                response.text,
+                "html.parser"
+            )
+
+            text = soup.get_text(
+                separator=" ",
+                strip=True
+            )
+
+            pages_content.append(text)
+
+            base_domain = urlparse(url).netloc
+
+            for link in soup.find_all("a", href=True):
+
+                next_url = urljoin(
+                    current_url,
+                    link["href"]
+                )
+
+                if urlparse(next_url).netloc == base_domain:
+                    crawl(next_url)
+
+        except Exception as e:
+            print(e)
+
+    crawl(url)
+
+    return pages_content
